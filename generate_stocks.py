98029e6ffd3e92ed2a3a13cfdf5028a27a6392dc
Tanner Papenfuss <tjpapenfuss@gmail.com>
2025-04-11 08:25:21 -0700

    stock data generation enhancement

---
+++ generate_stocks.py
@@ -0,0 +1,13 @@
+from pickle_files.generate_stock_data import generate_stock_data
+
+
+if __name__ == '__main__':
+    tickers_source='C:/Users/tjpap/sandbox/investment_forecasting/data/custom_allocation.csv'
+    ticker_data, spy_data = generate_stock_data(
+        tickers_source=tickers_source, 
+        start_date="2020-01-01", 
+        end_date="2022-01-01", 
+        interval="1d", 
+        top_n=50, 
+        to_pickle=True
+    )
